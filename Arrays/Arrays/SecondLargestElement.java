964448e9ff5ce337c2d17129c142e5e77b1828c2
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-17 12:08:33 +0530

    Create SecondLargestElement.java

---
+++ Arrays/Arrays/SecondLargestElement.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println(secondLargest);
+    }
+}
