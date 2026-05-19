538f6808b326c1e357466f464ccbbbc86837038e
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-19 09:23:37 +0530

    Create CountEvenNumbers.java

---
+++ Arrays/CountEvenNumbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++){
+		    arr[i] = scn.nextInt();
+		}
+	   int count = 0;
+		for(int i = 0;i<n;i++){
+		    if(arr[i]%2==0){
+		       count++;
+		    }
+		}
+		System.out.print(count);
+	}
+}
