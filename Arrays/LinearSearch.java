0bd1db0c7258cac43c5d9e71b82ff9e294810a56
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-19 09:27:05 +0530

    Create LinearSearch.java

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int x = scn.nextInt();
+        int index = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.print(index);
+    }
+}
