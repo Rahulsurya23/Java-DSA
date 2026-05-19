027c603d1dd5b7725b1ea4aed9ab5076ab42fbe9
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-19 09:21:39 +0530

    Create MinimumElement.java

---
+++ Arrays/Arrays/MinimumElement.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++){
+		    arr[i] = scn.nextInt();
+		}
+		int min = Integer.MAX_VALUE;
+		for(int i = 0;i<n;i++){
+		    if(arr[i]<min){
+		        min = arr[i];
+		    }
+		}
+		System.out.print(min);
+	}
+}
