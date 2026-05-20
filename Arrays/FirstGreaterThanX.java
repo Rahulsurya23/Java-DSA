ab06215045a3966d853e0c1844c597529ef6f9a0
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-20 05:46:07 +0530

    Create FirstGreaterThanX.java

---
+++ Arrays/FirstGreaterThanX.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++){
+		    arr[i] = scn.nextInt();
+		}
+		int ans = -1;
+		int x = scn.nextInt();
+		for(int i = 0;i<n;i++){
+		    if(arr[i]>x){
+		        ans = arr[i];
+		        break;
+		    }
+		}
+		System.out.print(ans);
+	}
+}
