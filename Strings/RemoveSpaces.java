6eed6e2ad22cc46994a1213dc3e3e338dd2a72ac
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-20 05:41:40 +0530

    Add RemoveSpaces.java to remove spaces from input

---
+++ Strings/RemoveSpaces.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		String str = scn.nextLine();
+		int n = str.length();
+	    String ans = str.replace(" ", "");
+	    System.out.print(ans);
+	}
+}
