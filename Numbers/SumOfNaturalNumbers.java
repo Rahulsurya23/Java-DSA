b8dd6c3a0036403a1322b9835b658011ded322a6
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-21 06:22:20 +0530

    Create SumOfNaturalNumbers.java

---
+++ Numbers/SumOfNaturalNumbers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int sum = 0;
+		for(int i = 1;i<=n;i++){
+		    sum+=i;
+		}
+		System.out.print(sum);
+	
+	}
+}
