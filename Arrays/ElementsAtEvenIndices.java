0acb2857931cb70149d21f12d476c350721985fc
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-21 06:14:56 +0530

    Create ElementsAtEvenIndices.java

---
+++ Arrays/ElementsAtEvenIndices.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++){
+		    arr[i] = scn.nextInt();
+		}
+		int ans = 0;
+		for(int i = 0;i<n;i++){
+		    if(i%2==0){
+		      	System.out.print(arr[i]+" ");
+		    }
+		}
+	
+	}
+}
