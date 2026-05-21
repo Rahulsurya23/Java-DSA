32e4abc0a69da75387b6ded0e1668f494e545a2e
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-21 06:10:14 +0530

    Create MaxMinDifference.java

---
+++ Arrays/MaxMinDifference.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++){
+		    arr[i] = scn.nextInt();
+		}
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for(int i = 0;i<n;i++){
+		    if(arr[i]>max){
+		        max = arr[i];
+		    }
+		}
+		for(int i = 0;i<n;i++){
+		    if(arr[i]<min){
+		        min = arr[i];
+		    }
+		}
+		
+		int ans = (max-min);
+		System.out.print(ans);
+		
+	}
+}
