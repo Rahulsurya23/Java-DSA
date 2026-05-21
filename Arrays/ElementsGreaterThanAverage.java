a902221b1449f3ed10e91802a084073e67d4984d
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-21 06:17:59 +0530

    Add ElementsGreaterThanAverage.java to find elements

---
+++ Arrays/ElementsGreaterThanAverage.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++){
+		    arr[i] = scn.nextInt();
+		}
+		int sum = 0;
+		for(int i = 0;i<n;i++){
+		   sum+=arr[i];
+		}
+		double avg = (double) sum/n;
+		for(int i = 0;i<n;i++){
+		    if(arr[i]>avg){
+		        System.out.print(arr[i]+" ");
+		    }
+		}
+	
+	}
+}
