46edbc71bb4c3d4429e8d74db72bbafc69135b08
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-21 06:40:30 +0530

    Create SecondSmallest.java

---
+++ Arrays/SecondSmallest.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++){
+		    arr[i] = scn.nextInt();
+		}
+		int min = Integer.MAX_VALUE;
+		int secondmin = Integer.MAX_VALUE;
+		for(int i = 0;i<n;i++){
+		    if(arr[i]<min){
+		        secondmin = min;
+		        min = arr[i];
+		    }
+		    else if(arr[i]<secondmin && arr[i]!=min){
+		        secondmin = arr[i];
+		    }
+		}
+		System.out.print(secondmin);
+	}
+}
