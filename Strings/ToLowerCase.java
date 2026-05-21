a456e0d089e504e1fb213d3f7f59bc61eb637e56
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-21 07:36:39 +0530

    Create ToLowerCase.java

---
+++ Strings/ToLowerCase.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		String str = scn.nextLine();
+		String ans = str.toLowerCase();
+		System.out.print(ans);
+		
+	}
+}
