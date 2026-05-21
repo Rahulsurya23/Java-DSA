936e85be841b8d10e5644dd07c60e091651bc51e
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-21 07:40:54 +0530

    Create PrintCharacters.java

---
+++ Strings/PrintCharacters.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+	    String str = scn.nextLine();
+	    int n = str.length();
+	    for(int i = 0;i<n;i++){
+	        System.out.println(str.charAt(i));
+	    }
+		
+	}
+}
