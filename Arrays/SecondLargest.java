155a5075fb43e78401ad9133c1a5b5f6899d677a
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-22 09:04:17 +0530

    Create SecondLargest.java

---
+++ Arrays/SecondLargest.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++){
+		    arr[i] = scn.nextInt();
+		}
+	    int largest = Integer.MIN_VALUE;
+	    int secondLargest = Integer.MIN_VALUE;
+		for(int i = 0;i<n;i++){
+		  if(arr[i]>largest){
+		      secondLargest = largest;
+		      largest = arr[i];
+		  }
+		  else if(arr[i]>secondLargest && arr[i]!=largest){
+		      secondLargest = arr[i];
+		  }
+		}
+		System.out.print(secondLargest);
+	}
+}
