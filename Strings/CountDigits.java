fcd4edccb9375a40888a37f5405d8595d525e5f1
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-22 09:09:45 +0530

    Create CountDigits.java

---
+++ Strings/CountDigits.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		String str = scn.nextLine();
+		int n = str.length();
+		 int count = 0;
+		for(int i = 0;i<n;i++){
+		    	char ch = str.charAt(i);
+		    	 
+	    if(ch>='0' && ch<='9'){
+	        count++;
+	    }
+		}
+	    System.out.print(count);
+	}
+}
