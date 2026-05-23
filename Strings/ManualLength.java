99fd96ac224b2fd06f6207cdfeea228b49be5f79
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-23 08:50:21 +0530

    Create ManualLength.java

---
+++ Strings/ManualLength.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner scn = new Scanner(System.in);
+    String str = scn.nextLine();
+    int n = str.length();
+    int count = 0;
+    for(char ch : str.toCharArray()){
+       count++;
+	}
+	System.out.print(count);
+}
+}
