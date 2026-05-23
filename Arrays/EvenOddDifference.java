5bba2e11bcaeb0884c7a4535a5bd455543122a75
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-23 07:38:34 +0530

    Create EvenOddDifference.java

---
+++ Arrays/EvenOddDifference.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner scn = new Scanner(System.in);
+	int n = scn.nextInt();
+	int[] arr = new int[n];
+	for(int i = 0;i<n;i++){
+	    arr[i] = scn.nextInt();
+	}
+	int even = 0;
+	int odd = 0;
+	for(int i = 0;i<n;i++){
+	    if(arr[i]%2==0){
+	        even+=arr[i];
+	    }else{
+	        odd+=arr[i];
+	    }
+	}
+	System.out.print((even-odd));
+
+	}
+}
