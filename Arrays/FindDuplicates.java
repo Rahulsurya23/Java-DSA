0f34f1c5c5ae5ed4f84ed55b310a07a5e85be778
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-24 09:31:13 +0530

    Create FindDuplicates.java

---
+++ Arrays/FindDuplicates.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0;i<n;i++){
+		    arr[i] = scn.nextInt();
+		}
+		boolean found = false;
+	
+		for(int i = 0;i<n;i++){
+		    for(int j = i+1;j<n;j++){
+		        if(arr[i]==arr[j]){
+		            System.out.print(arr[i]+" ");
+		          found = true;
+		          break;
+		        }
+		    }
+		}
+	     if(!found){
+		    System.out.print("No duplicate found");
+		}
+
+	}
+}
