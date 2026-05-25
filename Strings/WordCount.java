ac67d1afaa1a74423350d8c015782b4d7a8f74c9
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-25 10:13:23 +0530

    Create WordCount.java

---
+++ Strings/WordCount.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner scn = new Scanner(System.in);
+    String str = scn.nextLine();
+    String[] words = str.split(" ");
+      System.out.print(words.length);
+   }
+}
