52b15696301bfde270a6de2faa66007c1eb50ad1
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-25 10:02:10 +0530

    Create SquareElements.java

---
+++ Arrays/SquareElements.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner scn = new Scanner(System.in);
+	int n = scn.nextInt();
+	int[] arr = new int[n];
+	for(int i = 0;i<n;i++){
+	    arr[i] = scn.nextInt();
+	}
+	
+	for(int i = 0;i<n;i++){
+	    arr[i] = arr[i]*arr[i];
+	}
+for(int i = 0;i<n;i++){
+    System.out.print(arr[i]+" ");
+}
+   }
+}
