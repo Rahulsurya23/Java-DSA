70d90527bb3323f07bddff4df1cff6847212d937
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-26 09:29:34 +0530

    Create SumPrimeElements.java

---
+++ Arrays/SumPrimeElements.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  
+	 Scanner scn = new Scanner(System.in);
+	 int n = scn.nextInt();
+	 int[] arr = new int[n];
+	 for(int i = 0;i<n;i++){
+	     arr[i] = scn.nextInt();
+	 }
+	 int sum = 0;
+	 for(int i = 0;i<n;i++){
+	     if(isPrime(arr[i])){
+	         sum+=arr[i];
+	     }
+	}
+	System.out.print(sum);
+	}
+	public static boolean isPrime(int n){
+	    if(n<=1){
+	        return false;
+	    }
+	    for(int i = 2;i<=n/2;i++){
+	        if(n%i==0){
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+}
