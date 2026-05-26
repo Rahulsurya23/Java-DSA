3e5a351b2a28f8e927156931eb67d7e6b21edb44
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-26 09:43:03 +0530

    Create EvenOddIndexDifference.java

---
+++ Arrays/EvenOddIndexDifference.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  
+	 Scanner scn = new Scanner(System.in);
+	 int n = scn.nextInt();
+	 int[] arr = new int[n];
+	 for(int i = 0;i<n;i++){
+	     arr[i] = scn.nextInt();
+	 }
+	 int Evensum = 0;
+	 int oddSum = 0;
+	 for(int i = 0;i<n;i++){
+	     if(i%2==0){
+	         Evensum+=arr[i];
+	     }else{
+	         oddSum+=arr[i];
+	     }
+	}
+	int ans = Evensum-oddSum;
+	System.out.print(ans);
+	}
+}
