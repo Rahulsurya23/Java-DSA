38e21e8a63b46a4636fed28d806a0944d41c27ab
Rahulsurya <beesettyrahulsurya@gmail.com>
2026-05-26 10:04:58 +0530

    Create CountSpecialCharacters.java

---
+++ Strings/CountSpecialCharacters.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  
+	 Scanner scn = new Scanner(System.in);
+     String str = scn.nextLine();
+     int n = str.length();
+     int count = 0;
+     for(int i = 0;i<n;i++){
+         char ch = str.charAt(i);
+         if(!(ch>='a' && ch<='z') &&
+            !(ch>='A' && ch<='Z') &&
+            !(ch>='0' && ch<='9')&&
+            ch!=' '){
+             count++;
+         }
+     }
+     System.out.print(count);
+	}
+}
